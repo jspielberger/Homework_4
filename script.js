1107910ca867ccd86802bbabda3d0a81f4083eb1
Joel Spielberger <joelspielberger101@gmail.com>
2020-01-26 20:24:31 -0800

    figured out a lot of functionality and learned a few new ways of implementing arrays as well as cycling through questions, still need to implement the high score form

--- script.js
+++ script.js
@@ -1,34 +1,115 @@
-let score = 0;
-
-
-// The array of questions for our quiz game.
-var questions = [
-  { q: "The sky is blue.", a: "t" },
-  { q: "There are 365 days in a year.", a: "t" },
-  { q: "There are 42 ounces in a pound.", a: "f" },
-  { q: "The Declaration of Independence was created in 1745.", a: "f" },
-  { q: "Bananas are vegetables.", a: "f" }
-];
-
-// We start the game with a score of 0.
-var score = 0;
-
-// Loop over every question object
-for (var i = 0; i < questions.length; i++) {
-  // Display current question to user and ask OK/Cancel
-  var answer = confirm(questions[i].q);
-
-  // Compare answers
-  if ((answer === true && questions[i].a === "t") ||
-    (answer === false && questions[i].a === "f")) {
-    // Increase score
-    score++;
-    alert("Correct!");
-  }
-  else {
-    alert("Wrong!");
-  }
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
 }
 
-// Show total at end
-alert("You got " + score + "/" + questions.length);
+let secondsLeft = 90;
+
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+        secondsLeft += 10;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+//
+let timeEl = document.querySelector(".time");
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till end of Spider quiz.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      showScores();
+    }
+
+  }, 1000);
+}
+//
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+        //incorporate the name signing thing into show scores function?
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+        setTime();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+    //incorporate the score addition form (no default...)
+};
+
+
+
+
+// create questions here
+let questions = [
+    new Question("Who directed Spider-Man 2?", ["Sam Raimi", "Stanley Kubrick","Jonathan Demme", "Kathryn Bigelow"], "Sam Raimi"),
+    new Question("What is Spider-Man's secret identity?", ["Cuba Gooding Jr.", "Flash Thompson", "Peter Parker", "Jerry Seinfeld"], "Peter Parker"),
+    new Question("In the original comic book series, who was the first villain Spider-Man faced?", ["Stilt Man", "The Chameleon","The Vulture", "Rhino"], "The Chameleon"),
+    new Question("In which New York City borough was Spider-Man born and raised? (I excluded Staten Island because, why bother?) ", ["Brooklyn", "The Bronx", "Manhattan", "Queens"], "Queens"),
+    new Question("Who was the artist that originated the design and look of Spider-Man?", ["John Romita", "Jack Kirby", "Steve Ditko", "Frank Miller"], "Steve Ditko")
+];
+
+// create quiz
+let quiz = new Quiz(questions);
+
+// display quiz
+populate();
